376875d37ef30b5ca16a742e9445c4c665115f6e
luc1912 <lr53884@fer.hr>
2023-10-29 21:13:19 +0100

    inicijalni backend commit

---
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ForAllTheDogsBackendApplication.kt
@@ -0,0 +1,11 @@
+package hr.fer.progi.forAllTheDogsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForAllTheDogsBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForAllTheDogsBackendApplication>(*args)
+}
