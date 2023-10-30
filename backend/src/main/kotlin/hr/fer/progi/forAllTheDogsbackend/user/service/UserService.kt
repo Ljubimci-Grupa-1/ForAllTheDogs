04375bdaa93d10665c3b3e24d28b8230faa9b299
VMoskov <vm54341@fer.hr>
2023-10-30 19:42:55 +0100

    custom exception handler

--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/user/service/UserService.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/user/service/UserService.kt
@@ -1,5 +1,6 @@
 package hr.fer.progi.forAllTheDogsbackend.user.service
 
+import hr.fer.progi.forAllTheDogsbackend.exceptionHandler.ExceptionHandler
 import hr.fer.progi.forAllTheDogsbackend.user.controller.dto.AddUserDTO
 import hr.fer.progi.forAllTheDogsbackend.user.controller.dto.UserDTO
 import hr.fer.progi.forAllTheDogsbackend.user.repository.UserRespository
@@ -13,11 +14,12 @@ class UserService(
 ) {
     fun addUser(addUserDTO: AddUserDTO): UserDTO {
         var user = userRepository.findByEmail(addUserDTO.email)
-        if(user != null) throw IllegalArgumentException("nemože taj email")
+        if(user != null) throw IllegalArgumentException("Korisnik s tim emailom već postoji")
         user = userRepository.findByUsername(addUserDTO.username)
-        if(user != null) throw IllegalArgumentException("nemože taj username")
+        if(user != null) throw IllegalArgumentException("Korisnik s tim usernameom već postoji")
         user = userRepository.findByTelephoneNumber(addUserDTO.telephoneNumber)
-        if(user != null) throw IllegalArgumentException("nemože taj broj")
+        if(user != null) throw IllegalArgumentException("Broj mobitela već u uporabi")
+
         return UserDTO(
             userRepository.save(
                 addUserDTO.toUser()
