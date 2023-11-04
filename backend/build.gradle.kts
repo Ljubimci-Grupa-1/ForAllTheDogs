4c232f7f83fc4a7c070d4ef07a8764cc0e18a8aa
VMoskov <vm54341@fer.hr>
2023-11-04 09:10:45 +0100

    promjena sa H2 na PostreSQL bazu

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,7 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	runtimeOnly("com.h2database:h2")
+//	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
