a731e7cd6fe6ebe8408ab00ef478649905f1c480
VMoskov <vm54341@fer.hr>
2023-11-05 23:14:57 +0100

    dodan UserTypeRepository

---
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/userType/repository/UserTypeRepository.kt
@@ -0,0 +1,7 @@
+package hr.fer.progi.forAllTheDogsbackend.userType.repository
+
+import hr.fer.progi.forAllTheDogsbackend.userType.entity.UserType
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserTypeRepository: JpaRepository<UserType, Long> {
+}
