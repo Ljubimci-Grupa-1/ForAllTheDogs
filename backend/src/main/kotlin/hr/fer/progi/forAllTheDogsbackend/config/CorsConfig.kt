ca260fb2f2d61f981b2cb7a08fe49bde46ee1785
VMoskov <vm54341@fer.hr>
2023-11-08 20:02:54 +0100

    Promijenjen CorsConfig

--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/config/CorsConfig.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/config/CorsConfig.kt
@@ -1,30 +1,23 @@
+package hr.fer.progi.forAllTheDogsbackend.config
+
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
-import org.springframework.web.filter.CorsFilter
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class CorsConfig {
+
     @Bean
-    fun corsFilter(): CorsFilter {
-        val source = UrlBasedCorsConfigurationSource()
-        val config = CorsConfiguration()
-
-        // Allow requests from a specific origin during development
-        config.allowedOrigins = listOf("http://localhost:5173")
-
-        // Allow common HTTP methods
-        config.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
-
-        // Allow common headers
-        config.allowedHeaders = listOf("Content-Type", "Authorization")
-
-        // Allow credentials (e.g., cookies) to be sent with the request
-        config.allowCredentials = true
-
-        source.registerCorsConfiguration("/**", config)
-
-        return CorsFilter(source)
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**") // You can restrict paths with "/api/**" for example
+                    .allowedOrigins("http://localhost:5173") // The front-end server's address
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
     }
 }
