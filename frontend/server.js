fff179f261478f6c4e52aece15d339d49ac0f9d1
Anyrevlis <105887989+Anyrevlis@users.noreply.github.com>
2023-11-16 17:16:06 +0100

    CORS fixes

--- frontend/server.js
+++ frontend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
+app.use(cors());
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
