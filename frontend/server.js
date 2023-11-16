402cbcdbcea0c744811b38dec48269211af295e5
Anyrevlis <105887989+Anyrevlis@users.noreply.github.com>
2023-11-16 17:21:18 +0100

    CORS fixes v2

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const corsOptions = {
+    origin: 'https://forallthedogs.onrender.com/',
+};
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
