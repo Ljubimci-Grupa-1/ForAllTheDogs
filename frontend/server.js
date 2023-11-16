8c0569b3eb0676ed646a1f16b52cc3c64e818d22
Anyrevlis <105887989+Anyrevlis@users.noreply.github.com>
2023-11-16 16:21:28 +0100

    Deploy error fix v6

---
+++ frontend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
