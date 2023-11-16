9ac35f524cfef450c2606a635f2348b1a97af4c1
Anyrevlis <105887989+Anyrevlis@users.noreply.github.com>
2023-11-16 17:04:20 +0100

    Fetch fixes

--- frontend/src/components/LogIn/LogInForm.tsx
+++ frontend/src/components/LogIn/LogInForm.tsx
@@ -33,7 +33,7 @@ const LoginForm: FC = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:8080/user/login', {
+            const response = await fetch('https://forallthedogs.onrender.com//user/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
