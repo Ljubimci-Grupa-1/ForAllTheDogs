c0817aa452380ca9f54137994d0010acb557808f
Borna Josipović <bj53901@fer.hr>
2023-11-17 16:08:06 +0100

    Changes to login page error

--- frontend/src/components/MainContent/Bars/NavigationBar.tsx
+++ frontend/src/components/MainContent/Bars/NavigationBar.tsx
@@ -1,25 +1,36 @@
 import './NavigationBar.css';
-import {Button, Grid, Sheet} from "@mui/joy";
+import {Button, ButtonGroup, Grid, Sheet} from "@mui/joy";
+import {Link} from "react-router-dom";
 
 const NavigationBar = () => {
     return (
         <nav className="navbar">
             <Grid container spacing={3} sx={{flexGrow:1}}>
                 <Grid xs>
-                    <Sheet>
+                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
                         <Button>Login</Button>
                         <Button>Signup</Button>
                     </Sheet>
                 </Grid>
                 <Grid xs={6}>
-                    <Sheet>
-                        <a href="/map">Map</a>
-                        <a href="/account">Account</a>
-                        <a href="/inbox">Inbox</a>
-                    </Sheet>
+                    {/*<Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)',*/}
+                    {/*    justifyContent: "space-between"}}>*/}
+                        <ButtonGroup
+                            color="primary"
+                            disabled={false}
+                            orientation="horizontal"
+                            size="lg"
+                            spacing={3}
+                            variant="soft"
+                        >
+                            <Button component={Link} to="/map">Map</Button>
+                            <Button component={Link} to="/account">Account</Button>
+                            <Button component={Link} to="/inbox">Inbox</Button>
+                        </ButtonGroup>
+                    {/*</Sheet>*/}
                 </Grid>
                 <Grid xs>
-                    <Sheet>
+                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
                         <Button>Sign out</Button>
                     </Sheet>
                 </Grid>
