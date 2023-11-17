bb920768030d4f07a1ad86d6cc598b96a2dc7316
Borna Josipović <bj53901@fer.hr>
2023-11-17 16:15:09 +0100

    Changes to login page error

--- frontend/src/components/MainContent/Bars/NavigationBar.tsx
+++ frontend/src/components/MainContent/Bars/NavigationBar.tsx
@@ -7,14 +7,14 @@ const NavigationBar = () => {
         <nav className="navbar">
             <Grid container spacing={3} sx={{flexGrow:1}}>
                 <Grid xs>
-                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}>
                         <Button>Login</Button>
                         <Button>Signup</Button>
                     </Sheet>
                 </Grid>
                 <Grid xs={6}>
-                    {/*<Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)',*/}
-                    {/*    justifyContent: "space-between"}}>*/}
+                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0)',
+                        justifyContent: "space-between"}}>
                         <ButtonGroup
                             color="primary"
                             disabled={false}
@@ -27,10 +27,10 @@ const NavigationBar = () => {
                             <Button component={Link} to="/account">Account</Button>
                             <Button component={Link} to="/inbox">Inbox</Button>
                         </ButtonGroup>
-                    {/*</Sheet>*/}
+                    </Sheet>
                 </Grid>
                 <Grid xs>
-                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0.5)' }}>
+                    <Sheet sx={{ backgroundColor: 'rgba(255, 255, 255, 0)' }}>
                         <Button>Sign out</Button>
                     </Sheet>
                 </Grid>
