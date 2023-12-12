f98ed013d320bf5f2d2b8ff70a7c93fdb2243338
VMoskov <vm54341@fer.hr>
2023-12-12 22:36:19 +0100

    Added Location entity

---
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/location/entity/Location.kt
@@ -0,0 +1,23 @@
+package hr.fer.progi.forAllTheDogsbackend.location.entity
+
+import hr.fer.progi.forAllTheDogsbackend.city.entity.City
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
+
+@Entity
+class Location (
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "location_seq")
+    @SequenceGenerator(name = "location_seq", sequenceName = "location_seq", allocationSize = 1)
+    @Column(name = "locationid")
+    var locationId: Long = 0L,
+
+    @NotNull
+    var longitude: Double,
+
+    @NotNull
+    var latitude: Double,
+
+    @ManyToOne
+    var city: City
+)
