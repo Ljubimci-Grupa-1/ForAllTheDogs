38170bd43b727977a15bf44bdc3405665e9fcdad
VMoskov <vm54341@fer.hr>
2023-12-17 14:54:31 +0100

    Dodana paginacija za dohvaćanje svih oglasa

--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/controller/AdController.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/controller/AdController.kt
@@ -1,13 +1,15 @@
 package hr.fer.progi.forAllTheDogsbackend.ad.controller
 
+import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AdDTO
 import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AddAdDTO
+import hr.fer.progi.forAllTheDogsbackend.ad.entity.Ad
 import hr.fer.progi.forAllTheDogsbackend.ad.service.AdService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/ad")
 @Controller
@@ -17,6 +19,16 @@ class AdController(private val adService: AdService) {
     fun getAllAds() = ResponseEntity.ok(
         adService.getAllAds()
     )
+
+    @GetMapping("/all/pageable")
+    fun getAllAds(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "10") size: Int
+    ): ResponseEntity<Page<AdDTO>> {
+        val pageable: Pageable = PageRequest.of(page, size)
+        val ads: Page<AdDTO> = adService.getAllAds(pageable)
+        return ResponseEntity.ok(ads)
+    }
 
     @PostMapping("/add")
     fun addAd(@RequestBody addAdDTO: AddAdDTO) = ResponseEntity.ok(
