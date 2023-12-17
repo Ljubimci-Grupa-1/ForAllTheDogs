7c467a1340538b4b94644526d1cf7a3a03c02db3
VMoskov <vm54341@fer.hr>
2023-12-17 13:57:02 +0100

    Dodana mogućnost dohvaćanja svih oglasa

--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/controller/AdController.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/controller/AdController.kt
@@ -4,6 +4,7 @@ import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AddAdDTO
 import hr.fer.progi.forAllTheDogsbackend.ad.service.AdService
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/ad")
 @Controller
 class AdController(private val adService: AdService) {
+
+    @GetMapping("/all")
+    fun getAllAds() = ResponseEntity.ok(
+        adService.getAllAds()
+    )
 
     @PostMapping("/add")
     fun addAd(@RequestBody addAdDTO: AddAdDTO) = ResponseEntity.ok(
--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/service/AdService.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/service/AdService.kt
@@ -5,13 +5,15 @@ import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AdDTO
 import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AddAdDTO
 import hr.fer.progi.forAllTheDogsbackend.ad.repository.AdRepository
 import hr.fer.progi.forAllTheDogsbackend.city.repository.CityRepository
-import hr.fer.progi.forAllTheDogsbackend.color.entity.Color
 import hr.fer.progi.forAllTheDogsbackend.color.repository.ColorRepository
-import hr.fer.progi.forAllTheDogsbackend.image.entity.Image
+import hr.fer.progi.forAllTheDogsbackend.image.controller.dto.ImageDTO
 import hr.fer.progi.forAllTheDogsbackend.image.repository.ImageRepository
+import hr.fer.progi.forAllTheDogsbackend.location.controller.dto.LocationDTO
 import hr.fer.progi.forAllTheDogsbackend.location.repository.LocationRepository
+import hr.fer.progi.forAllTheDogsbackend.pet.controller.dto.PetDTO
 import hr.fer.progi.forAllTheDogsbackend.pet.repository.PetRepository
 import hr.fer.progi.forAllTheDogsbackend.species.repository.SpeciesRepository
+import hr.fer.progi.forAllTheDogsbackend.user.controller.dto.UserAdDTO
 import hr.fer.progi.forAllTheDogsbackend.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import org.springframework.web.multipart.MultipartFile
@@ -37,7 +39,47 @@ class AdService(
     private val colorRepository: ColorRepository,
     private val speciesRepository: SpeciesRepository
 ) {
-    fun addAd(dto: AddAdDTO): String {
+
+    fun getAllAds(): List<AdDTO> {
+        val ads = adRepository.findAll()
+        val adDTOs = mutableListOf<AdDTO>()
+
+        for(ad in ads) {
+            // getting all the necessary data from the database
+            val user = userRepository.findById(ad.user.userId)
+            val activity = activityRepository.findById(ad.activity.activityId)
+            val pet = petRepository.findById(ad.pet.petId)
+            val city = cityRepository.findById(pet.get().location.city.cityId)
+            val location = locationRepository.findById(pet.get().location.locationId)
+            val species = speciesRepository.findById(pet.get().species.speciesId)
+
+            // mapping the colors to a set of strings with the color names
+            val colors = ad.pet.colors.map { color ->
+                color.colorName
+            }.toMutableSet()
+
+            // mapping the images to a list of ImageDTOs
+            val images = imageRepository.findByAd(ad).toList().map { image ->
+                ImageDTO(image)
+            }
+
+            // adding the ad to the list of DTOs
+            adDTOs.add(
+                AdDTO(ad, UserAdDTO(user.get()),
+                    activity.get().activityCategory,
+                    PetDTO(pet.get(), colors, species.get(),
+                        LocationDTO(location.get(), city.get().cityName)
+                    ),
+                    images
+                )
+            )
+        }
+
+        return adDTOs
+    }
+
+    fun addAd(dto: AddAdDTO): AdDTO {
+        // getting all the necessary data from the DTO
         val activity = activityRepository.findByActivityCategory(dto.activityName) ?:
             throw IllegalArgumentException("Ne postoji kategorija s imenom ${dto.activityName}!")
 
@@ -45,7 +87,7 @@ class AdService(
             throw IllegalArgumentException("Ne postoji korisnik s emailom ${dto.user.email}!")
 
         val city = cityRepository.findByCityName(dto.pet.location.cityName) ?:
-        throw IllegalArgumentException("Ne postoji grad ${dto.pet.location.cityName}!")
+            throw IllegalArgumentException("Ne postoji grad ${dto.pet.location.cityName}!")
 
         val location = locationRepository.save(dto.pet.location.toLocation(city))
 
@@ -57,18 +99,31 @@ class AdService(
         val species = speciesRepository.findBySpeciesName(dto.pet.speciesName) ?:
             throw IllegalArgumentException("Ne postoji vrsta životinje ${dto.pet.speciesName}!")
 
-
+        // saving the pet, and ad to the database
         val pet = petRepository.save(dto.pet.toPet(location, species, colors))
 
         val ad = adRepository.save(dto.toAd(activity, pet, user))
 
-        for(image in dto.images) {
+        // saving the compressed images to the database
+        dto.images.forEach { image ->
             imageRepository.save(dto.toImage(compressImage(image), ad, null))
         }
 
-        return "Uspjeh"
+        // returning the DTO with the saved data
+        return AdDTO(ad, UserAdDTO(user),
+            activity.activityCategory,
+            PetDTO(pet,
+                dto.pet.colors.map { color -> color.colorName }.toMutableSet(), // map the colors to a set of strings
+                species,
+                LocationDTO(location, city.cityName)
+            ),
+            // map the images to a list of ImageDTOs
+            dto.images.map { image -> ImageDTO(dto.toImage(compressImage(image), ad, null)) }
+        )
     }
 
+
+//    HELPER FUNCTIONS
 
     private fun compressImage(photo: MultipartFile): ByteArray {
         val MAX_SIZE: Long = 5 * 1024 * 1024 // 5 MB
