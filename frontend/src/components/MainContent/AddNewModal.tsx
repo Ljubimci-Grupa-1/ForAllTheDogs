0802635be80f97b6c5f27f44b5a0a1ad216c33ee
Borna Josipović <bj53901@fer.hr>
2023-12-18 14:00:18 +0100

    Added date and time picker

--- frontend/src/components/MainContent/AddNewModal.tsx
+++ frontend/src/components/MainContent/AddNewModal.tsx
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from "react";
 import "./AddNewModal.css"
 import {Box, Chip, Input, Option, Select, Sheet, Stack} from "@mui/joy";
+import dayjs from 'dayjs';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+
 
 export const AddNewModal = () =>{
     const [colors, setColors] = useState([]);
@@ -97,6 +102,36 @@ export const AddNewModal = () =>{
                                         </Option>
                                     ))}
                                 </Select>
+                            </div>
+                            <div className="input-container">
+                                <label htmlFor="dateanddtime">Date and time:</label>
+                                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <MobileDateTimePicker
+                                            variant="soft"
+                                            sx={{
+                                                '& .MuiPaper-root': {
+                                                    backgroundColor:'black',
+                                                },
+                                                '& .MuiInputBase-input': {
+                                                    height: '50%',
+                                                    border: '0px',
+                                                    margin: '0px',
+                                                    backgroundColor:'#97C3F0',
+
+                                                },
+                                                height: '50%',
+                                                backgroundColor:'black',
+                                                '& .MuiInputBase-root': {
+                                                    height: '100%',
+                                                    backgroundColor:'black',
+
+                                                },
+                                            }}
+                                            id="dateanddtime" defaultValue={dayjs('2022-04-17T15:30')} />
+                                </LocalizationProvider>
+                            </div>
+                            <div className="input-container">
+
                             </div>
 
                         </Stack>
