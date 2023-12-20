4ec22a012c7209f1e5a99584ba7ed6310c565b0b
VMoskov <vm54341@fer.hr>
2023-12-20 14:01:11 +0100

    added editAd method

--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/service/AdService.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/ad/service/AdService.kt
@@ -3,6 +3,7 @@ package hr.fer.progi.forAllTheDogsbackend.ad.service
 import hr.fer.progi.forAllTheDogsbackend.activity.repository.ActivityRepository
 import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AdDTO
 import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.AddAdDTO
+import hr.fer.progi.forAllTheDogsbackend.ad.controller.dto.EditAdDTO
 import hr.fer.progi.forAllTheDogsbackend.ad.entity.Ad
 import hr.fer.progi.forAllTheDogsbackend.ad.repository.AdRepository
 import hr.fer.progi.forAllTheDogsbackend.city.repository.CityRepository
@@ -11,7 +12,9 @@ import hr.fer.progi.forAllTheDogsbackend.image.controller.dto.ImageDTO
 import hr.fer.progi.forAllTheDogsbackend.image.repository.ImageRepository
 import hr.fer.progi.forAllTheDogsbackend.location.controller.dto.LocationDTO
 import hr.fer.progi.forAllTheDogsbackend.location.repository.LocationRepository
+import hr.fer.progi.forAllTheDogsbackend.pet.controller.dto.EditPetDTO
 import hr.fer.progi.forAllTheDogsbackend.pet.controller.dto.PetDTO
+import hr.fer.progi.forAllTheDogsbackend.pet.entity.Pet
 import hr.fer.progi.forAllTheDogsbackend.pet.repository.PetRepository
 import hr.fer.progi.forAllTheDogsbackend.species.repository.SpeciesRepository
 import hr.fer.progi.forAllTheDogsbackend.user.controller.dto.UserAdDTO
@@ -58,8 +61,6 @@ class AdService(
         val city = cityRepository.findByCityName(dto.pet.location.cityName) ?:
             throw IllegalArgumentException("Ne postoji grad ${dto.pet.location.cityName}!")
 
-        val location = locationRepository.save(dto.pet.location.toLocation(city))
-
         val colors = dto.pet.colors.map { color ->
             colorRepository.findByColorName(color.colorName)
                 ?: throw IllegalArgumentException("Ne postoji boja ${color.colorName}!")
@@ -68,7 +69,9 @@ class AdService(
         val species = speciesRepository.findBySpeciesName(dto.pet.speciesName) ?:
             throw IllegalArgumentException("Ne postoji vrsta životinje ${dto.pet.speciesName}!")
 
-        // saving the pet, and ad to the database
+        // saving the pet, location and ad to the database
+        val location = locationRepository.save(dto.pet.location.toLocation(city))
+
         val pet = petRepository.save(dto.pet.toPet(location, species, colors))
 
         val ad = adRepository.save(dto.toAd(activity, pet, user))
@@ -89,6 +92,19 @@ class AdService(
             // map the images to a list of ImageDTOs
             dto.images.map { image -> ImageDTO(dto.toImage(compressImage(image), ad, null)) }
         )
+    }
+
+    fun editAd(id: Long, editAddDTO: EditAdDTO): AdDTO {
+        val ad = adRepository.findById(id).get()
+        val pet = petRepository.findById(ad.pet.petId).get()
+
+        editPetDetails(pet, editAddDTO.pet ?: EditPetDTO())
+        petRepository.save(pet)
+
+        editAdDetails(ad, editAddDTO)
+        adRepository.save(ad)
+
+        return createAdDTO(ad)
     }
 
 
@@ -121,6 +137,38 @@ class AdService(
             ),
             images
         )
+    }
+
+    private fun editPetDetails(pet: Pet, editPetDTO: EditPetDTO) {
+        pet.species = editPetDTO.speciesName?.let { speciesRepository.findBySpeciesName(it) } ?: pet.species
+        if(editPetDTO.location != null) {
+            val longitude = editPetDTO.location.longitude ?: pet.location.longitude
+            val latitude = editPetDTO.location.latitude ?: pet.location.latitude
+            val city = editPetDTO.location.cityName?.let { cityRepository.findByCityName(it) } ?: pet.location.city
+            val location = locationRepository.save(editPetDTO.location.toLocation(city, longitude, latitude))
+            pet.location = location
+        }
+        if(editPetDTO.colors != null) {
+            val colors = editPetDTO.colors.map { color ->
+                colorRepository.findByColorName(color.colorName)
+                    ?: throw IllegalArgumentException("Ne postoji boja ${color.colorName}!")
+            }.toMutableSet()
+            pet.colors = colors
+        }
+        pet.petName = editPetDTO.petName ?: pet.petName
+        pet.age = editPetDTO.age ?: pet.age
+        pet.dateTimeMissing = editPetDTO.dateTimeMissing ?: pet.dateTimeMissing
+        pet.description = editPetDTO.description ?: pet.description
+    }
+
+    private fun editAdDetails(ad: Ad, editAdDTO: EditAdDTO) {
+        ad.inShelter = editAdDTO.inShelter ?: ad.inShelter
+        ad.activity = editAdDTO.activityName?.let { activityRepository.findByActivityCategory(it) } ?: ad.activity
+        if (editAdDTO.images != null) {
+            editAdDTO.images.forEach { image ->
+                imageRepository.save(editAdDTO.toImage(compressImage(image), ad, null))
+            }
+        }
     }
 
     private fun compressImage(photo: MultipartFile): ByteArray {
