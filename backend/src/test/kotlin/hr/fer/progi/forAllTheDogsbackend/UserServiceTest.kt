2fbb7e89ea98aaf967d0a59ba6fa961a4b8afae1
luc1912 <lr53884@fer.hr>
2024-01-15 15:54:11 +0100

    login valid unit test

---
+++ backend/src/test/kotlin/hr/fer/progi/forAllTheDogsbackend/UserServiceTest.kt
@@ -0,0 +1,63 @@
+package hr.fer.progi.forAllTheDogsbackend
+
+import hr.fer.progi.forAllTheDogsbackend.user.controller.dto.LoginUserDTO
+import hr.fer.progi.forAllTheDogsbackend.user.entity.User
+import hr.fer.progi.forAllTheDogsbackend.user.repository.UserRepository
+import hr.fer.progi.forAllTheDogsbackend.user.service.UserService
+import hr.fer.progi.forAllTheDogsbackend.userType.entity.UserType
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+
+@SpringBootTest
+class UserServiceTest {
+
+    @MockBean
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var userService: UserService
+
+    @Test
+    fun loginValidTest(){
+        val newUser = User(
+            userId = 1L,
+            username = "username",
+            email = "us.us@us.us",
+            password = "ususus123",
+            name = "user",
+            telephoneNumber = "1234567899",
+            userType = UserType(1L, "Osoba")
+        )
+
+        val login = LoginUserDTO(
+            newUser.email,
+            newUser.password
+        )
+
+        `when`(userRepository.findByEmail(newUser.email)).thenReturn(newUser)
+
+        val savedUser: User = userService.authorizeUser(login).toUser()
+        println(savedUser.email + " "
+                + savedUser.password + " "
+                + savedUser.userId  + " "
+                + savedUser.userType.name + " "
+                + savedUser.userType.userTypeId + " "
+                + savedUser.telephoneNumber
+        )
+
+        assertNotNull(savedUser)
+        assertEquals(newUser, savedUser)
+    }
+
+    @Test
+    fun loginInvalidTest(){
+
+    }
+
+
+}
