27e154138171f0946aa6d33f07bf0078156d93bd
VMoskov <vedran.moskov@fer.hr>
2024-01-15 16:56:41 +0100

    added equals and hashCode to JsonUserDTO

--- backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/user/controller/dto/JsonUserDTO.kt
+++ backend/src/main/kotlin/hr/fer/progi/forAllTheDogsbackend/user/controller/dto/JsonUserDTO.kt
@@ -8,4 +8,28 @@ data class JsonUserDTO (
     val name: String,
     val telephoneNumber: String,
     val userTypeId: Long
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is JsonUserDTO) return false
+
+        if (username != other.username) return false
+        if (email != other.email) return false
+        if (password != other.password) return false
+        if (name != other.name) return false
+        if (telephoneNumber != other.telephoneNumber) return false
+        if (userTypeId != other.userTypeId) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = username.hashCode()
+        result = 31 * result + email.hashCode()
+        result = 31 * result + password.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + telephoneNumber.hashCode()
+        result = 31 * result + userTypeId.hashCode()
+        return result
+    }
+}
