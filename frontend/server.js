5ea9f8eec1d5b25f49560331ec8787879e429955
Anyrevlis <105887989+Anyrevlis@users.noreply.github.com>
2024-01-16 15:53:52 +0100

    Deploy edits

--- frontend/server.js
+++ frontend/server.js
@@ -1,22 +1,21 @@
-// const express = require('express');
-// const path = require('path');
-// const cors = require('cors');
-// const corsOptions = {
-//     origin: 'https://forallthedogs-omw5.onrender.com',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-// };
-// const app = express();
-//
-// app.use(cors(corsOptions));
-//
-// app.use(express.static(path.join(__dirname, 'dist')));
-//
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
-//
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//     console.log(`Server running on http://localhost:${port}`);
-// });
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const corsOptions = {
+    origin: 'https://forallthedogs-omw5.onrender.com',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
+const app = express();
+
+app.use(cors(corsOptions));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
